e61b0c2e25e97461f944f682ef676468ebac3b1e
JagadeeswaranG <jagadees.vg@gmail.com>
2022-11-22 15:15:33 +0530

    add

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
     // api : "http://localhost:3002"
-    api : "https://git.heroku.com/money-manager-backend-webcode.git"
+    api : "https://money-manager-backend-webcode.herokuapp.com/"
 }
